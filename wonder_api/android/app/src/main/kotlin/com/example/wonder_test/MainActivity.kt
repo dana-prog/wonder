459e43daced37546bfa8376a2b6fb097fc6aa7c3
dana <danashalev100@gmail.com>
2025-07-05 17:45:36 +0300

    reorg wonder modules

--- wonder_api/android/app/src/main/kotlin/com/example/wonder_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.wonder.api_playground
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- wonder_widgetbook/android/app/src/main/kotlin/com/example/wonder_widgetbook/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.wonder.widgetbook
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
